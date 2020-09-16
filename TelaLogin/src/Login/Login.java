ae2f342821474b76d9443cbf0037716c70df3e26
Alessandra <alessandra.guedes@outlook.com>
2020-09-16 14:34:43 -0300

    Primeira versão do prototipo de telas, tipo: Primeiro commit

---
+++ TelaLogin/src/Login/Login.java
@@ -0,0 +1,21 @@
+package Login;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame{
+    public Login()
+    {
+        setTitle("Seja Bem-Vindo");
+        setSize(360,300);
+        add(p_panel);
+    }
+
+    private JButton btn_login;
+    private JPanel p_panel;
+    private JTextField textField1;
+    private JTextField textField2;
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Login.Login;
+
+public class Main {
+    public static void main (String[] args)
+    {
+        Login telaLogin = new Login();
+        telaLogin.setVisible(true);
+    }
+
+}
