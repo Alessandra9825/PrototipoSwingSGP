f452061e61c4457f103dbb61a2b461dbb394ba2c
Guilherme Mecenero <guilherme.machado@saebrasil.org.br>
2020-09-21 19:09:44 -0300

    Criado Tela Finança

---
+++ TelaFinanca/src/Financa/TelaFinanca.java
@@ -0,0 +1,12 @@
+package Financa;
+
+import javax.swing.*;
+
+public class TelaFinanca {
+    private JLabel lblValSaldo;
+    private JButton btnAddReceita;
+    private JButton btnAddGastos;
+    private JButton btnAddContaMensal;
+    private JButton btnAddGastosCartao;
+    private JTable tblTransicoes;
+}
